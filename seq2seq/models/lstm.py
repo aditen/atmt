c958aeb2d429ebc2b9a2763ce7c02d91a6639b50
Adrian Iten <adrian.iten@competec.ch>
2021-11-13 22:44:46 +0100

    Add (not so good) droput results

--- seq2seq/models/lstm.py
+++ seq2seq/models/lstm.py
@@ -25,8 +25,8 @@ class LSTMModel(Seq2SeqModel):
         parser.add_argument('--encoder-hidden-size', type=int, help='encoder hidden size')
         parser.add_argument('--encoder-num-layers', type=int, help='number of encoder layers')
         parser.add_argument('--encoder-bidirectional', help='bidirectional encoder')
-        parser.add_argument('--encoder-dropout-in', help='dropout probability for encoder input embedding')
-        parser.add_argument('--encoder-dropout-out', help='dropout probability for encoder output')
+        parser.add_argument('--encoder-dropout-in', type=float, help='dropout probability for encoder input embedding')
+        parser.add_argument('--encoder-dropout-out', type=float, help='dropout probability for encoder output')
 
         parser.add_argument('--decoder-embed-dim', type=int, help='decoder embedding dimension')
         parser.add_argument('--decoder-embed-path', help='path to pre-trained decoder embedding')
@@ -124,7 +124,7 @@ class LSTMEncoder(Seq2SeqEncoder):
         src_embeddings = _src_embeddings.transpose(0, 1)
 
         # Pack embedded tokens into a PackedSequence
-        packed_source_embeddings = nn.utils.rnn.pack_padded_sequence(src_embeddings, src_lengths)
+        packed_source_embeddings = nn.utils.rnn.pack_padded_sequence(src_embeddings, src_lengths.cpu())
 
         # Pass source input through the recurrent layer(s)
         packed_outputs, (final_hidden_states, final_cell_states) = self.lstm(packed_source_embeddings)
